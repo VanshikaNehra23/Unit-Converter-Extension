fcfca6ad0a519533d124a5b0279584267d067502
Arnav <arnavpunit@gmail.com>
2021-03-14 15:55:19 +0530

    Fixed lib bug and replaced seetting icon

--- content.js
+++ content.js
@@ -40,6 +40,26 @@
 //console.log(getTextNodesIn(document.body, false));
 
 
+var fractions = {
+	'¼': 0.25,
+	'½': 0.5,
+	'¾': 0.75,
+	'⅐': 0.1428,
+	'⅑': 0.111,
+	'⅒': 0.1,
+	'⅓': 0.333,
+	'⅔': 0.667,
+	'⅕': 0.2,
+	'⅖': 0.4,
+	'⅗': 0.6,
+	'⅘': 0.8,
+	'⅙': 0.167,
+	'⅚': 0.8333,
+	'⅛': 0.125,
+	'⅜': 0.375,
+	'⅝': 0.625,
+	'⅞': 0.875
+};
 
 
 var x = document.getElementsByTagName('p');
@@ -49,8 +69,13 @@ var paras = [];
 for(var i = 0; i< x.length; i++)
 {
     var str = x[i].innerText;
+    processElement(str)
     paras.push(str);
 }
 console.log(paras);
 
+function processElement(str)
+{
+
+}
 //chrome.runtime.openOptionsPage()
